2d1c39db4e3ccbf13d6737d43582d910423e30c1
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2024-02-15 14:49:59 +0100

    Templates adjsuted so it is usable on mobile devices

--- core/middleware.py
+++ core/middleware.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib import messages
 
 
 class RedirectIfLoggedInMiddleware:
@@ -8,6 +9,7 @@ class RedirectIfLoggedInMiddleware:
 
     def __call__(self, request):
         if request.user.is_authenticated and request.path == reverse("login_user"):
+            messages.success(request, ("You're already logged in or registered"))
             return redirect("index")
         response = self.get_response(request)
         return response
