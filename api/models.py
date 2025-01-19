b114458dec0051223cc1768a13eaecc863923311
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2025-01-19 16:32:35 +0100

    BTCUSDHourly model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BTCUSDHourly(models.Model):
+    time = models.BigIntegerField()
+    high = models.FloatField()
+    low = models.FloatField()
+    open = models.FloatField()
+    volumefrom = models.FloatField()
+    volumeto = models.FloatField()
+    close = models.FloatField()
+
+    
